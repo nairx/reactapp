e8e471d47e41ff4a1647602eb33963e718109c66
nairx <nairxprv@gmail.com>
2025-05-01 12:03:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+// import './App.css'
+function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8081/products").then((res) =>
+      res.json().then((data) => setProducts(data))
+    );
+  }, []);
+  return (
+    <>
+      <h1>React App</h1>
+      <div>
+        {products && products.map(value=>(
+          <li key={value.id}>{value.id}-{value.name}-{value.price}</li>
+        ))}
+      </div>
+    </>
+  );
+}
+export default App;
